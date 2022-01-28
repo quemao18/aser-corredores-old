4b9275e3879862ecd18c345fb44b97be937235c7
Alejandro Toba <alejandro.toba@gmail.com>
2022-01-28 09:30:08 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ const web = 'http://localhost:4200';
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAgQJTAqrjG5Oh0NfO6PzRMXmC-e6zMykI",
+    apiKey: "",
     authDomain: "aser-corredores.firebaseapp.com",
     databaseURL: "https://aser-corredores.firebaseio.com",
     projectId: "aser-corredores",
